20e89a394dc0af59361b4870fb5c3d70661c7e2a
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-05-28 18:54:55 +0500

    Реализованы контроллеры для домашней страницы и контактов

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'main/index.html')
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        print(name, email, phone)
     return render(request, 'main/contacts.html')
