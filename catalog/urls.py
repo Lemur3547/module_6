f787a4c9743b6be5ad8cc98d463825ff5123bedc
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-06-11 23:35:08 +0500

    Настроены ссылки

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from catalog.views import index, contacts, product
 
 urlpatterns = [
-    path('', index),
-    path('contacts/', contacts),
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
     path('product/<int:pk>/', product, name='name_url')
 ]
