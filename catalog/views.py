dd7b8b79ec4d1c60cd2b4543c6ef9d7149668a6b
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-06-11 18:47:46 +0500

    Создан шаблон и контроллер для отображения страницы товара

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,7 @@
+from django.db.models import Model
 from django.shortcuts import render
+
+from catalog.models import Product
 
 
 def index(request):
@@ -12,3 +15,10 @@ def contacts(request):
         phone = request.POST.get('phone')
         print(name, email, phone)
     return render(request, 'main/contacts.html')
+
+
+def product(request, pk):
+    context = {
+        'object': Product.objects.get(pk=pk)
+    }
+    return render(request, 'main/product.html', context)
