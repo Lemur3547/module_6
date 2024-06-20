c21b2f4db4e5bd33c5018eb695b5b371c2a268e2
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-06-20 18:51:44 +0500

    Контроллеры переведены с FBV на CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from catalog.views import index, contacts, product, add_product
+from catalog.views import ProductListView, ProductDetailView, ProductCreateView, contacts
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', ProductListView.as_view(), name='index'),
+    # path('contacts/', ContactsView.as_view(), name='contacts'),
     path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>/', product, name='name_url'),
-    path('add_product/', add_product, name='add_product')
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='name_url'),
+    path('add_product/', ProductCreateView.as_view(), name='add_product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, View, UpdateView, DeleteView
 
-from catalog.models import Product, Category
+from catalog.models import Product, BlogPost
 
 
-def index(request):
-    context = {
-        'products_list': Product.objects.all()
-    }
-    return render(request, 'main/index.html', context)
+class ProductListView(ListView):
+    model = Product
+
+
+class ContactsView(View):
+    template_name = 'catalog/contacts.html'
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+            print(name, email, phone)
+        return render(request, 'catalog/contacts.html')
 
 
 def contacts(request):
@@ -16,25 +27,14 @@ def contacts(request):
         email = request.POST.get('email')
         phone = request.POST.get('phone')
         print(name, email, phone)
-    return render(request, 'main/contacts.html')
+    return render(request, 'catalog/contacts.html')
 
 
-def product(request, pk):
-    context = {
-        'object': Product.objects.get(pk=pk)
-    }
-    return render(request, 'main/product.html', context)
+class ProductDetailView(DetailView):
+    model = Product
 
 
-def add_product(request):
-    context = {
-        'category_list': Category.objects.all()
-    }
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        description = request.POST.get('description')
-        preview = request.FILES.get('preview')
-        category = request.POST.get('category')
-        price = request.POST.get('price')
-        Product.objects.create(name=name, description=description, preview=preview, category=Category.objects.get(pk=category), price=price)
-    return render(request, 'main/add_product.html', context)
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'preview', 'category', 'price')
+    success_url = reverse_lazy('catalog:index')
