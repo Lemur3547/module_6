fc6b45a7cfeac2392f2c1be24c59d936ebfa0180
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-07-02 19:32:34 +0500

    Добавлена работа с формами для версий продукта

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from catalog.models import Product
+from catalog.models import Product, Version
 
 
 class ProductForm(forms.ModelForm):
@@ -27,3 +27,9 @@ class ProductForm(forms.ModelForm):
                 raise forms.ValidationError('В описании содержится запрещенное слово')
 
         return cleaned_data
+
+
+class VersionForm(forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = '__all__'
