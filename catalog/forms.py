028699e942bee740949a1cd458d2f08238a4d155
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-07-02 20:07:20 +0500

    К формам применены стили

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,20 @@
 from django import forms
+from django.forms import BooleanField
 
 from catalog.models import Product, Version
 
 
-class ProductForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+            
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
     achtung_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 
     class Meta:
@@ -29,7 +40,7 @@ class ProductForm(forms.ModelForm):
         return cleaned_data
 
 
-class VersionForm(forms.ModelForm):
+class VersionForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Version
         fields = '__all__'
