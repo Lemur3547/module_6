22e65481f1a281aa67a2d3fe983a375fe4a7c917
Emik Ubuntu <emikgal2507@gmail.com>
2024-07-23 20:33:19 +0500

    Добавлено кэширование всего сайта кроме некоторых страниц

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page, never_cache
 
 from catalog.views import ProductListView, ProductDetailView, ProductCreateView, PostCreateView, PostListView, \
     PostDetailView, PostUpdateView, PostDeleteView, ContactsView, ProductUpdateView, ProductDeleteView, CategoryView
@@ -8,15 +8,15 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product'),
-    path('add_product/', ProductCreateView.as_view(), name='add_product'),
-    path('edit_product/<int:pk>/', ProductUpdateView.as_view(), name='edit_product'),
+    path('add_product/', never_cache(ProductCreateView.as_view()), name='add_product'),
+    path('edit_product/<int:pk>/', never_cache(ProductUpdateView.as_view()), name='edit_product'),
     path('delete_product/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
 
     path('categories/', CategoryView.as_view(), name='categories'),
 
     path('blog/', PostListView.as_view(), name='blog'),
     path('blog/post/<int:pk>', PostDetailView.as_view(), name='post'),
-    path('blog/add_post/', PostCreateView.as_view(), name='add_post'),
-    path('blog/edit_post/<int:pk>', PostUpdateView.as_view(), name='edit_post'),
+    path('blog/add_post/', never_cache(PostCreateView.as_view()), name='add_post'),
+    path('blog/edit_post/<int:pk>', never_cache(PostUpdateView.as_view()), name='edit_post'),
     path('blog/delete_post/<int:pk>', PostDeleteView.as_view(), name='delete_post'),
 ]
